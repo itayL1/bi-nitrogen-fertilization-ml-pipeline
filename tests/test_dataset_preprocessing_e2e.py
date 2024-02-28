d3228dbffc64e253b5a6c0eaad06d4e3ccb9c3ef
itaylotan <itay.lotan@signalpet.com>
2024-02-28 18:26:18 +0200

    progress

--- tests/test_dataset_preprocessing_e2e.py
+++ tests/test_dataset_preprocessing_e2e.py
@@ -7,6 +7,7 @@ from bi_nitrogen_fertilization_ml_pipeline.core.data_classes.train_session_conte
 from bi_nitrogen_fertilization_ml_pipeline.core.dataset_preprocessing import dataset_preprocessing
 from tests.utils.test_datasets import load_Nitrogen_with_Era5_and_NDVI_dataset, \
     default_Nitrogen_with_Era5_and_NDVI_dataset_features_config
+from sklearn.model_selection import train_test_split
 
 
 def _init_train_session_context(features_config: FeaturesConfig) -> TrainSessionContext:
@@ -22,14 +23,19 @@ def _init_train_session_context(features_config: FeaturesConfig) -> TrainSession
 
 def test_dataset_preprocessing_e2e():
     # Arrange
-    raw_train_dataset_df = load_Nitrogen_with_Era5_and_NDVI_dataset()
+    raw_dataset_df = load_Nitrogen_with_Era5_and_NDVI_dataset()
     train_session_context = _init_train_session_context(
         features_config=default_Nitrogen_with_Era5_and_NDVI_dataset_features_config(),
     )
+    raw_train_dataset_df, raw_inference_dataset_df =\
+        train_test_split(raw_dataset_df, test_size=0.2, random_state=42)
 
     # Act
-    preprocessed_inference_dataset_df = dataset_preprocessing.train_dataset_preprocessing(
+    preprocessed_train_dataset_df = dataset_preprocessing.train_dataset_preprocessing(
         raw_train_dataset_df, train_session_context)
+    preprocessed_inference_dataset_df = dataset_preprocessing.inference_dataset_preprocessing(
+        raw_inference_dataset_df, training_artifacts=train_session_context.artifacts)
 
     # Assert
+    print(preprocessed_train_dataset_df.shape)
     print(preprocessed_inference_dataset_df.shape)
