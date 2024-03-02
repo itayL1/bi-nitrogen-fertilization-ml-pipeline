88667d04b54c09380d98bb637efbc08bd6eb9984
itayL1 <itay235711@gmail.com>
2024-03-02 19:34:51 +0200

    Training module implementation (#3)
    Training module implementation

--- bi_nitrogen_fertilization_ml_pipeline/core/data_classes/train_params.py
+++ bi_nitrogen_fertilization_ml_pipeline/core/data_classes/train_params.py
@@ -1,9 +1,17 @@
 from typing import Optional, Callable
 
-from pydantic import validator
+from pydantic import validator, confloat, PositiveInt, Field
+from keras.optimizers import Optimizer
 
+from bi_nitrogen_fertilization_ml_pipeline.core import defaults
 from bi_nitrogen_fertilization_ml_pipeline.core.data_classes.base_model import BaseModel
+from bi_nitrogen_fertilization_ml_pipeline.core.data_classes.evaluation_functions import EvaluationFunctions
 from bi_nitrogen_fertilization_ml_pipeline.core.data_classes.field_utils import not_empty_str
+
+
+class TrainEarlyStoppingSettings(BaseModel):
+    validation_set_fraction_size: confloat(ge=0.0, lt=1.0)
+    tolerance_epochs_count: PositiveInt
 
 
 class EvaluationFoldsKeySettings(BaseModel):
@@ -21,5 +29,10 @@ class EvaluationFoldsKeySettings(BaseModel):
 
 
 class TrainParams(BaseModel):
+    loss_function: EvaluationFunctions = Field(default=defaults.LOSS_FUNCTION)
+    evaluation_metric: EvaluationFunctions = Field(default=defaults.EVALUATION_METRIC)
+    optimizer: Optimizer = Field(default_factory=defaults.ADAM_OPTIMIZER)
+    epochs_count: int
+    early_stopping: Optional[TrainEarlyStoppingSettings]
     evaluation_folds_key: EvaluationFoldsKeySettings
     random_seed: Optional[int]
--- bi_nitrogen_fertilization_ml_pipeline/model_training/model_evaluation.py
+++
@@ -1,4 +0,0 @@
-def year_based_k_fold_cross_validate_model(
-
-):
-    pass
